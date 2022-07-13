36a0b6441f6f560e412bba274880a6c2b917066e
dizhongdi <2755063993@qq.com>
2022-07-13 21:05:58 +0800

    Gateway网关

---
+++ infrastructure/api-gateway/src/main/java/com/dizhongdi/apigateway/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.dizhongdi.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * ClassName:CorsConfig
+ * Package:com.dizhongdi.apigateway.config
+ * Description:
+ *
+ * @Date: 2022/7/13 20:04
+ * @Author:dizhongdi
+ */
+//网关解决跨域问题
+//@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
